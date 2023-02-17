5a4b42bc7541715ef63df8b0aa3a22812f366f5d
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-16 20:01:59 -0500

    included addNote and deleteNoteById into notes.js

--- db/notes.js
+++ db/notes.js
@@ -1,30 +1,45 @@
-const util = require('util')
-const fs = require('fs')
-const uuid = require('uuid/v1')
-const readFile = util.promisify(fs.readFile)
-const writeFile = util.promisify(fs.writeFile)
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid/v1');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 class Notes {
     read(){
-        return readFile("db/db.json", "utf-8")
+        return readFile('./db/db.json', "utf-8");
     }
 
-    write(note){
-        return writeFile("db/db.json", JSON.stringify(note))
+    write(notes){
+        return writeFile('./db/db.json', JSON.stringify(notes));
     }
 
     getAllNotes(){
         return this.read().then((notes) => {
             let allNotes;
             try{
-                allNotes = [].concat(JSON.parse(notes))
+                allNotes = [].concat(JSON.parse(notes));
             }
             catch(err){
                 allNotes = []
             }
             return allNotes
-        })
+        });
+    }
+
+    addNote(note) {
+        return this.getAllNotes().then((notes) => {
+          const newNotes = [...notes, note];
+          return this.write(newNotes).then(() => note);
+        });
+    }
+
+    deleteNoteById(id) {
+        return this.getAllNotes().then((notes) => {
+          const newNotes = notes.filter((note) => note.id !== id);
+          return this.write(newNotes);
+        });
     }
 }
 
-module.exports = new Notes()
+module.exports = new Notes();
